719264c47967eeb0b2e904675a23ae64592ca699
charlie <151513835+charlie-git-hub@users.noreply.github.com>
2024-03-25 11:51:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,15 +2,6 @@
 document.querySelector('#popup').style.display = 'block';
 
 
-var elements = document.getElementsById('spoiler');
-elements[i].addEventListener('click', function(e) {
-    var elements = document.getElementsByClassName('img_accueil');
-    for (var i = 0; i < elements.length; i++) {
-            this.style.filter = 'blur(1.5rem)';
-            document.getElementById('popup-close').addEventListener('click', function(e) {
-                document.getElementById('popup').style.display = 'none'})          
-        }});
-
 
     document.querySelector('#popup-close').onclick = e => {
         e.preventDefault();
@@ -18,3 +9,5 @@ elements[i].addEventListener('click', function(e) {
         popup.style.display = 'none';
         azertyuiop();
     }
+
+
